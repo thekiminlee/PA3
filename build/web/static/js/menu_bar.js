5e29fb787fbafae08ff13d471ccb74147ba0e249
Ki Min Lee <kimin@dhcp-v083-109.mobile.uci.edu>
2018-05-22 11:47:56 -0700

    Commit complete application

---
+++ build/web/static/js/menu_bar.js
@@ -0,0 +1,10 @@
+function indexMenu(){
+    var menu = "<div class=\"navbar\">" +
+		"<a href=\"index.html\">PEDAL WORLD</a>"+
+		"<a href=\"product\">Products</a>"+
+		"<a href=\"about_us.html\">About Us</a>"+
+                "<a href=\"cart\" style='float: right; padding-right: 25px;'>Check Out</a>" +
+	"</div>";
+    document.write(menu);
+    
+}
